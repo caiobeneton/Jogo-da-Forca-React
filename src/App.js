0c82d3bfe0cbfec9b1065bca34464479f7fc12ed
Caio Beneton <caio.beneton@gmail.com>
2022-09-26 12:30:41 -0300

    Vitoria e derrota finalizados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import forca0 from './assets/forca0.png'
-//import forca1 from './assets/forca1.png'
-//import forca2 from './assets/forca2.png'
-//import forca3 from './assets/forca3.png'
-//import forca4 from './assets/forca4.png'
-//import forca5 from './assets/forca5.png'
-//import forca6 from './assets/forca6.png'
+import forca1 from './assets/forca1.png'
+import forca2 from './assets/forca2.png'
+import forca3 from './assets/forca3.png'
+import forca4 from './assets/forca4.png'
+import forca5 from './assets/forca5.png'
+import forca6 from './assets/forca6.png'
 import palavras from './Palavras.js'
 
 
@@ -16,26 +16,56 @@ export default function App(){
     const [chutesFeitos, setChutesFeitos] = useState(alfabeto)
     const [erros, setErros] = useState(0)
     const [chutesCertos, setChutesCertos] = useState([])
+    const [status, setStatus] = useState('array')
+    const forcaImgs = [forca0, forca1, forca2, forca3, forca4, forca5, forca6]
+    let errosTotais;
+    let totalCertos;
 
     function sortearPalavra(){
         palavras.sort(() => 0.5 - Math.random())
         setPalavraSorteada(palavras[0])
         setJogoOFF(false)
         setChutesFeitos([])
+        setChutesCertos([])
+        setErros(0)
+        setStatus('array')
     }
 
     function chutarLetra(letra){
         setChutesFeitos([...chutesFeitos, letra])
         if (palavraLimpa.includes(letra)){
-            setChutesCertos([...chutesCertos, letra])
+            totalCertos = [...chutesCertos, letra]
+            setChutesCertos(totalCertos)
+            checaVitoria()
         } else {
-            setErros(erros + 1)
+            errosTotais = erros + 1
+            setErros(errosTotais)
+            checaErros()
         }
-
     }
+
+    function checaVitoria(){
+        if (palavraLimpa.every(letra => totalCertos.includes(letra))) {
+            setJogoOFF(true)
+            setChutesFeitos([...alfabeto])
+            setStatus('ganhou')
+        }
+    }
+
+    function checaErros(){
+        if (errosTotais === 6) {
+            setJogoOFF(true)
+            setChutesFeitos([...alfabeto])
+            setStatus('perdeu')
+            setChutesCertos([...palavraLimpa])
+        }
+    }
+    
+
 
     const palavraLimpa = palavraSorteada.normalize("NFD").replace(/\p{Diacritic}/gu, "").split('')
     const palavraMascarada = palavraLimpa.map((letra) => chutesCertos.includes(letra) ? letra : '_')
+    console.log(palavraLimpa)
     
     return(
         <>
@@ -43,11 +73,11 @@ export default function App(){
                 <div className="wrapper">
                     <div className="jogo">
                         <div className="imagem-forca">
-                            <img src={forca0} alt='' ></img>
+                            <img src={forcaImgs[erros]} alt='' ></img>
                         </div>
                         <div className="info-jogo">
                             <button onClick={sortearPalavra}>Escolher palavra</button>
-                            <div className='array'>{palavraMascarada.map((letra, idx) => <span key={idx}>{letra}</span>)}</div>
+                            <div className={status}>{palavraMascarada.map((letra, idx) => <span key={idx}>{letra}</span>)}</div>
                         </div>
                     </div>
                     <div className='teclado'>
